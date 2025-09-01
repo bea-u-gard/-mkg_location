072d815eb78bcdd09fa4859b858ab4ec24534466
wisdomfoli <wisdom.folithepotential.ai>
2025-09-01 16:59:02 +0000

    update

--- mkg_location/wsgi.py
+++ mkg_location/wsgi.py
@@ -10,7 +10,9 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mkg_location.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root='../staticfiles')
